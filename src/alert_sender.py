3f2ca2edccfbd2a2803e1f8021972e13145a6da8
alexanderstas <stasalexanderj@gmail.com>
2021-02-12 16:33:22 -0500

    Added data directory and updated codebase

--- src/alert_sender.py
+++ src/alert_sender.py
@@ -34,7 +34,7 @@ class EmailSender:
 
         sms = msg.as_string()
 
-        self.server.sendmail(gmail_user, self.sms_gateway[0], body)
+        self.server.sendmail(gmail_user, self.sms_gateway[1], body)
         self.server.quit()
 
 
@@ -46,7 +46,12 @@ def get_return_percentages():
 def write_holdings():
     with open(PATH_TO_HOLDINGS, 'w') as f:
         data = rs.build_holdings()
-        f.write(pprint.pformat(data))
+        f.write(json.dumps(data))
+
+def get_holdings():
+    with open(PATH_TO_HOLDINGS) as f:
+        data = f.read()
+        return json.loads(data)
 
 
 if __name__ == '__main__':
@@ -55,38 +60,37 @@ if __name__ == '__main__':
 
     return_percentages = get_return_percentages()
     write_holdings()
+    # portfolio = get_holdings()
 
-    portfolio = rs.build_holdings()
+    # symbols = [
+    #     {'ticker': 'POAI', 'min_target': 1.90, 'type': 'stock'},
+    #     {'ticker': 'SKLZ', 'min_target': 35.00, 'type': 'stock'},
+    #     {'ticker': 'CCIV', 'min_target': 30.00, 'type': 'stock'},
+    #     {'ticker': 'ZOM', 'max_target': 3.20, 'type': 'stock'}
+    # ]
 
-    for ticker, position in portfolio.items():
-        avg = float(position['average_buy_price'])
-        curr = float(position['price'])
-        percent_increase = (curr - avg) / avg * 100
-        print(ticker, avg, curr, str(round(percent_increase, 2)) + '%')
+    while True:
+        portfolio = rs.build_holdings()
+        for ticker, position in portfolio.items():
+            avg = float(position['average_buy_price'])
+            curr = float(position['price'])
+            percent_increase = (curr - avg) / avg * 100
+            if percent_increase >= return_percentages[ticker]:
+                print(f'Alert sent for {ticker}')
+                alert.send_email(ticker, f' has increased by {percent_increase}. www.robinhood.com/stocks/{ticker}')
+                time.sleep(1)
+        # for symbol in symbols:
+        #     current_price = float(rs.get_stock_quote_by_symbol(symbol['ticker'])['ask_price'])
+        #     if 'min_target' in symbol:
+        #         if current_price <= symbol['min_target']:
+        #             alert.send_email(symbol["ticker"],
+        #                              f'[{symbol["ticker"]}] has dropped to {symbol["min_target"]}.\nwww.robinhood.com/stocks/{symbol["ticker"]} '
+        #                              )
+        #             symbol['min_target'] = 0
+        #     else:
+        #         if current_price >= symbol['max_target']:
+        #             alert.send_email(symbol["ticker"],
+        #                              f'[{symbol["ticker"]}] has risen to {symbol["max_target"]}.\nwww.robinhood.com/stocks/{symbol["ticker"]} '
+        #                              )
+        #             symbol['max_target'] = float("inf")
 
-    # print(portfolio)
-    print(return_percentages)
-
-    symbols = [
-        {'ticker': 'POAI', 'min_target': 1.40, 'type': 'stock'},
-        {'ticker': 'SKLZ', 'min_target': 35.00, 'type': 'stock'},
-        {'ticker': 'CCIV', 'min_target': 30.00, 'type': 'stock'},
-        {'ticker': 'ZOM', 'max_target': 3.20, 'type': 'stock'}
-    ]
-
-    # while True:
-    #     for symbol in symbols:
-    #         current_price = float(rs.get_stock_quote_by_symbol(symbol['ticker'])['ask_price'])
-    #         if 'min_target' in symbol:
-    #             if current_price <= symbol['min_target']:
-    #                 alert.send_email(symbol["ticker"],
-    #                                  f'[{symbol["ticker"]}] has dropped to {symbol["min_target"]}.\nwww.robinhood.com/stocks/{symbol["ticker"]} '
-    #                                  )
-    #                 symbol['min_target'] = 0
-    #         else:
-    #             if current_price >= symbol['max_target']:
-    #                 alert.send_email(symbol["ticker"],
-    #                                  f'[{symbol["ticker"]}] has risen to {symbol["max_target"]}.\nwww.robinhood.com/stocks/{symbol["ticker"]} '
-    #                                  )
-    #                 symbol['max_target'] = float("inf")
-    #         time.sleep(5)
